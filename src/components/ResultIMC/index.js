2ee68597ebdafde8c79d8756e53822ba4c40202b
Luan <luancpereira30@gmail.com>
2023-02-03 11:28:43 -0300

    button share implementado

--- src/components/ResultIMC/index.js
+++ src/components/ResultIMC/index.js
@@ -1,9 +1,24 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Share } from "react-native";
 import styles from "./style";
 export default function ResultIMC({ resultImc, messageResultImc }) {
+  const onShare = async () => {
+    const result = await Share.share({
+      message: `Meu imc hoje é: ${resultImc}`,
+    });
+  };
+
   return (
     <View style={styles.resultImc}>
+      <View style={styles.boxShareButton}>
+        {resultImc != null ? (
+          <TouchableOpacity onPress={onShare} style={styles.shared}>
+            <Text style={styles.sharedText}>Share</Text>
+          </TouchableOpacity>
+        ) : (
+          <View />
+        )}
+      </View>
       <Text style={styles.information}>{messageResultImc}</Text>
       <Text style={styles.numberImc}>{resultImc}</Text>
     </View>
